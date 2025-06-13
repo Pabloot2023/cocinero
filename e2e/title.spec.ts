84bbe67e37ccb7f54e52f1c5405ac513d89e2a3b
Pablo <pablo32laboral@gmail.com>
2025-06-13 20:51:16 -0300

    Fix: título correcto en test

--- e2e/title.spec.ts
+++ e2e/title.spec.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('should have correct title', async ({ page }) => {
   await page.goto('/');
-  await expect(page).toHaveTitle(/cocineroS/);
+  await expect(page).toHaveTitle(/cocinero/);
 }); 
